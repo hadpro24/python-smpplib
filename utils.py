09f56984d463424e82bed3decc689a7ec7efc09b
Harouna Diallo <dev.harouna@gmail.com>
2023-08-10 03:05:20 +0000

    change port and host redis

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ class PersistentSequenceGenerator(object):
     PERSISTENCE_NAME = 'provider_sequences'
 
     def __init__(self):
-        self.redis = redis.Redis(host='127.0.0.1', port=6399, db=4)
+        self.redis = redis.Redis(host='10.106.0.4', port=6379, db=4)
         actual_value = self.redis.get(self.PERSISTENCE_NAME)
         if actual_value:
             self._sequence = int(actual_value, 0)
