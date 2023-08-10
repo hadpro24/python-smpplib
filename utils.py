ce7ca4410dca29111899a1617037d92f2006836d
Harouna Diallo <dev.harouna@gmail.com>
2023-08-10 04:19:30 +0000

    change host name

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ class PersistentSequenceGenerator(object):
     PERSISTENCE_NAME = 'provider_sequences'
 
     def __init__(self):
-        self.redis = redis.Redis(host='provider_dev', port=6379, db=4)
+        self.redis = redis.Redis(host='worker_dev', port=6379, db=4)
         actual_value = self.redis.get(self.PERSISTENCE_NAME)
         if actual_value:
             self._sequence = int(actual_value, 0)
